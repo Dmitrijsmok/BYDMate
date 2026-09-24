65780bfdeef1eb07df9acf96362f216c2860f84c
Dmitry <dmitry@orangelv.com>
2026-09-24 17:49:30 +0300

    build: bump field build to 64026

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         // on DiLink Android 12 (requestLegacyExternalStorage works).
         // targetSdk 30+ would break listFiles() on /storage/emulated/0/energydata/
         targetSdk = 29
-        versionCode = 64025
+        versionCode = 64026
         versionName = "3.17.5"
         buildConfigField("String", "BUILD_ID", "\"$buildId\"")
 
