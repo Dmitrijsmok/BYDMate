8dd657713c9be886e1e08a739d85b988479763e6
Dmitry <2920454+Dmitrijsmok@users.noreply.github.com>
2026-09-26 20:18:19 +0300

    Remove duplicate detekt suppressions

--- app/src/main/kotlin/com/bydmate/app/voice/SherpaTtsEngine.kt
+++ app/src/main/kotlin/com/bydmate/app/voice/SherpaTtsEngine.kt
@@ -42,7 +42,6 @@ internal class TtsSamplesCallback(
  *  Each speak() supersedes queued/playing speech via a generation counter;
  *  stop() invalidates all pending jobs. */
 @Suppress("LongParameterList")
-@Suppress("LongParameterList")
 class SherpaTtsEngine(
     private val modelManager: TtsModelManager,
     private val selectedVoice: () -> TtsVoice = { TtsVoiceCatalog.byId(TtsModelManager.DEFAULT_VOICE_ID) },
@@ -622,7 +621,6 @@ class SherpaTtsEngine(
         }
 
         @Suppress("CyclomaticComplexMethod", "TooGenericExceptionCaught", "LoopWithTooManyJumpStatements")
-        @Suppress("CyclomaticComplexMethod", "LoopWithTooManyJumpStatements", "TooGenericExceptionCaught")
         private fun playbackLoop() {
             var totalFramesWritten = 0L
             try {
